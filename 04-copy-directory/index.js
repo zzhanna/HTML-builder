6052fa197c8b57001afdb55374b895ab4db8a43e
zzhanna <foto1drug@gmail.com>
2021-11-10 13:12:38 +0200

    fix: fix 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,17 +2,31 @@ const fs = require('fs');
 const path = require('path');
 const stdout = process.stdout;
 
-fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
-   if (err) throw err;
+fs.access(path.join(__dirname, 'files-copy'), err => {
+   if (!err) {
+      fs.rmdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+           if (err) throw err;
+      });
+       setTimeout(() => {dir()}, 500 ) ;
+    } else if (err) {
+           dir();
+   }
 });
-fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
-   if (err) throw err;
-   files.forEach(file => {
-      if (file.isFile()) {
-         fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
-            if (err) throw err;
-         });
-      };
+
+function dir() {
+   fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+      if (err) throw err;
    });
-   stdout.write('Your files have been copied in new directory files-copy \n')
-});
+   fs.readdir(path.join(__dirname, 'files'), { withFileTypes: true }, (err, files) => {
+      if (err) throw err;
+      files.forEach(file => {
+         if (file.isFile()) {
+            fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), (err) => {
+               if (err) throw err;
+            });
+         };
+         console.log('1')
+      });
+      stdout.write('Your files have been copied in new directory files-copy \n')
+   });
+}
